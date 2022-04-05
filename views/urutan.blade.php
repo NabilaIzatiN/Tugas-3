c9c59e7490d7f96b4d1db64613f89ae3186f57ed
NabilaIzatiN <87664031+NabilaIzatiN@users.noreply.github.com>
2022-04-05 23:32:01 +0700

    Add files via upload

---
+++ views/urutan.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+       < meta charset="UTF-8">
+       <meta name="viewport" content="width-device-wifth, initial-scale=1.0">
+       <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    </head>
+    <body>
+        urutan ke- {{$ke}}
+    </body>
+</html>
